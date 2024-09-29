504d0fec3383e410e43e6df9443a0bca5164775d
indraoctama <indra.octama@smartfren.com>
2024-09-29 16:09:24 +0700

    new db diagram

--- db/migrate/20240928152457_create_users.rb
+++ db/migrate/20240928152457_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[7.2]
     create_table :users do |t|
       t.string :username
       t.string :password
-      t.string :salt
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_29_084551) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.string "salt"
     t.string "password_digest"
   end
 
