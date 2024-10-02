f426a0144ea42f086e36b5d88ead9adda90d8746
indraoctama <indra.octama@smartfren.com>
2024-10-02 21:59:07 +0700

    endpoint for transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,20 @@
 class TransactionsController < ApplicationController
+  # Create a new transaction between two users
+  def create
+    sender = User.find(params[:sender_id])
+    receiver = User.find(params[:receiver_id])
+    amount = params[:amount].to_f
+
+    if amount <= 0
+      return render json: { error: "Amount must be greater than zero" }, status: :unprocessable_entity
+    end
+
+    begin
+      # Perform the transaction
+      sender.perform_transaction(receiver, amount, params[:description])
+      render json: { message: "Transaction successful", sender: sender.wallet, receiver: receiver.wallet }, status: :ok
+    rescue => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    end
+  end
 end
